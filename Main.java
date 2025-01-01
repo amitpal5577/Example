c4447d58dbb3c7152aa767fa4bd359b8caa934c6
amitpal5577 <apal5577@outlook.com>
2025-01-01 18:45:48 +0530

    added age conditon in java file

--- Main.java
+++ Main.java
@@ -4,5 +4,11 @@ public class Main {
         for (int i = 0; i<10; i++) {
             System.out.println("Hello World");
         }
+        int age = 19;
+        if(age> 18) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are a child");
+        }
     }
 }
