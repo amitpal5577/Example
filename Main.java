340d3e22cf1adecd0660e5a29f39b12e117086c8
Amit Pal <136923709+amitpal5577@users.noreply.github.com>
2025-04-18 17:48:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        /// this newbranch
         // i will print this message 10 time
         for (int i = 0; i<10; i++) {
             System.out.println("Hello World");
